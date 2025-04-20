e6745cdfe6ca3715c5645c8982ed9c2043926902
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-04-20 16:36:20 +0200

    remove(app): Remove comment + change language fr->en

--- src/app.py
+++ src/app.py
@@ -4,28 +4,24 @@ import os
 from streamlit_option_menu import option_menu
 from styles import get_css, get_github_button
 
-# Configuration de la page
-st.set_page_config(page_title="CHATBOT-IA", layout="wide")
+st.set_page_config(page_title="CHATBOT-AI", layout="wide")
 
-# Chargement des styles CSS
 st.markdown(get_css(), unsafe_allow_html=True)
 
-# Initialisation des variables de session
+
 def initialiser_session():
     if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "assistant", "content": "Comment puis-je vous aider aujourd'hui ?"}]
+        st.session_state.messages = [{"role": "assistant", "content": "How can I help you today?"}]
     if "temperature" not in st.session_state:
         st.session_state.temperature = 0.1
 
-# Fonction pour effacer l'historique de chat
-def effacer_historique_chat():
-    st.session_state.messages = [{"role": "assistant", "content": "Comment puis-je vous aider aujourd'hui ?"}]
 
-# Fonction pour générer une réponse
+def effacer_historique_chat():
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help you today?"}]
+
 def generer_reponse(prompt_input):
     selected_model = st.session_state.get('selected_model', 'llama2-7b')
     
-    # Sélection du modèle approprié
     modeles = {
         'llama2-7b': 'meta/llama-2-7b-chat:13c3cdee13ee059ab779f0291d29054dab00a47dad8261375654de5540165fb0',
         'llama3-8b-instruct': 'meta/meta-llama-3-8b-instruct',
@@ -37,7 +33,7 @@ def generer_reponse(prompt_input):
     llm = modeles.get(selected_model, modeles['llama2-7b'])
 
     try:
-        string_dialogue = "Tu es un assistant utile."
+        string_dialogue = "You are a helpful assistant."
         for msg in st.session_state.messages:
             role = "user" if msg["role"] == "user" else "assistant"
             string_dialogue += f"\n{role}: {msg['content']}"
@@ -47,15 +43,14 @@ def generer_reponse(prompt_input):
         output = replicate.run(llm, input={"prompt": f"{string_dialogue}\nassistant: {prompt_input}", "temperature": temperature})
         return output
     except Exception as e:
-        st.error(f"Erreur lors de la génération de la réponse: {str(e)}")
-        return "Désolé, une erreur s'est produite. Veuillez vérifier votre configuration et réessayer."
+        st.error(f" Error while generating the response: {str(e)}")
+        return "Sorry, an error occurred. Please check your configuration and try again"
 
-# Création de la barre latérale
 def creer_sidebar():
     with st.sidebar:
         selected_option = option_menu(
             menu_title="IA BOT",
-            options=["Chatbot", "Réglages", "À propos"],
+            options=["Chatbot", "Setting", "About"],
             icons=["chat", "gear", "info-circle"],
             menu_icon="cast",
             default_index=0,
@@ -64,7 +59,7 @@ def creer_sidebar():
     replicate_api = None
     
     if selected_option == "Chatbot":
-        st.sidebar.button('Effacer Chat', on_click=effacer_historique_chat)
+        st.sidebar.button('Clear Chat', on_click=effacer_historique_chat)
 
         selected_model = st.sidebar.selectbox(
             'Choisir un modèle',
@@ -72,38 +67,34 @@ def creer_sidebar():
             key='selected_model'
         )
 
-    elif selected_option == "Réglages":
-        st.sidebar.markdown("### Jeton API")
-        replicate_api = st.sidebar.text_input('Entrer le jeton API:', type='password', key='replicate_api_token')
+    elif selected_option == "Setting":
+        st.sidebar.markdown("### Replicate Token")
+        replicate_api = st.sidebar.text_input('Enter your token API:', type='password', key='replicate_api_token')
 
-        st.sidebar.markdown("### Température")
-        temperature = st.sidebar.slider("Régler la température", 0.0, 1.0, 0.1, 0.1)
+        st.sidebar.markdown("### Temperature")
+        temperature = st.sidebar.slider("Set the temperature", 0.0, 1.0, 0.1, 0.1)
         st.session_state.temperature = temperature
 
-    elif selected_option == "À propos":
-        st.sidebar.markdown("### À propos")
-        st.sidebar.markdown("Interagissez avec différents modèles Llama🦙\n\nEntrez votre jeton API Replicate dans les réglages pour commencer🚀")
+    elif selected_option == "About":
+        st.sidebar.markdown("### About")
+        st.sidebar.markdown("Interact with different trending LLM models\nEnter your Replicate API token in the settings to get started🚀")
         st.sidebar.markdown('<hr>', unsafe_allow_html=True)
         st.sidebar.markdown(get_github_button(), unsafe_allow_html=True)
 
     return replicate_api
 
-# Initialisation de la session
+
 initialiser_session()
 
-# Création de la barre latérale et récupération du jeton API
 replicate_api = creer_sidebar()
 
-# Configuration de l'API Replicate
 if replicate_api:
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-# Affichage de l'historique des messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Gestion de l'entrée utilisateur
 if prompt := st.chat_input(disabled=not replicate_api):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
